950137a178082f5b3afa903d218ff4ce186cbde6
ZUITOPIA <korhwngml123@naver.com>
2023-04-07 15:49:37 +0900

    Add Pen3 exercise

---
+++ exercise/Pen3.java
@@ -0,0 +1,52 @@
+// 1912943 Lab 1-3 실습
+
+public class Pen3 {
+    String vender;
+    String color;
+    int price;
+
+    public Pen3() {  }
+
+    public Pen3(String name){
+        vender = name;
+    }
+
+    public Pen3 (String name, String col, int pp){
+        vender = name;
+        color = col;
+        price = pp;
+    }
+
+    void write(){
+        System.out.println("Pen: write()");
+        System.out.println("Pen Vender : " + vender);
+        System.out.println(", Pen Color : " + color);
+        System.out.println(", Pen Price : " + price);
+    }
+
+    void write(int xx){
+        System.out.println("Pen: write(int xx)");
+        System.out.println("Pen Vender : " + vender);
+        System.out.println(", Pen Color : " + color);
+        System.out.println(", Pen Price : " + xx);
+    }
+
+    void write(int xx, String yy){
+        System.out.println("Pen: write(int xx)");
+        System.out.println("Pen Vender : " + yy);
+        System.out.println(", Pen Color : " + color);
+        System.out.println(", Pen Price : " + xx);
+    }
+
+    void write(int xx, String yy, String zz){
+        System.out.println("Pen: write(int xx)");
+        System.out.println("Pen Vender : " + yy);
+        System.out.println(", Pen Color : " + zz);
+        System.out.println(", Pen Price : " + xx);
+    }
+
+    void erase(){
+        System.out.println("Pen : erase()");
+    }
+}
+
