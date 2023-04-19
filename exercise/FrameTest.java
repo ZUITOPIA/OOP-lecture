7665acba4e912544b3f662be654d52afbd5297d9
ZUITOPIA <korhwngml123@naver.com>
2023-04-19 13:31:31 +0900

    Add exercise files

---
+++ exercise/FrameTest.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class FrameTest{
+    public static void main(String[] args){
+        Frame f = new Frame("FrameTest : Juhee Ko");
+        f.setSize(500,500);
+        f.setBackground(Color.red);
+        f.setVisible(true);
+    }
+}
---
+++ exercise/LabelMain.java
@@ -0,0 +1,5 @@
+public class LabelMain{
+    public static void main(String[] args){
+    new LabelTest();
+    }
+} // Main은 항상 따로 
